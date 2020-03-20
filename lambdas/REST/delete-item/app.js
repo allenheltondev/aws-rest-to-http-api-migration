512c0c66b42846eddb82cc1542caa2c27ee93b70
Helton, Allen <allen.helton@tylertech.com>
2020-03-20 08:24:19 -0500

    Initial Commit - REST

---
+++ lambdas/REST/delete-item/app.js
@@ -0,0 +1,29 @@
+const httpStatusCode = require('http-status-codes');
+
+exports.lambdaHandler = async (event, context) => {
+  const id = event.pathParameters.itemId;
+
+  await deleteItemFromDynamo(id);
+
+  return {
+    statusCode: httpStatusCode.NO_CONTENT,
+    body: '',
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  };
+};
+
+async function deleteItemFromDynamo(id) {
+  try {
+    const params = {
+      TableName: process.env.TableName,
+      Key: {
+        pk: id
+      }
+    };
+
+    await documentClient.delete(params).promise();
+  } catch (err) {
+    console.log('An error occurred deleting item from Dynamo');
+    console.log(err);
+  }
+}
